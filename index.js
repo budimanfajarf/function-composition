c480255044e0b20f491e50aed5d4b78169b20e05
Budi <budimanfajarf@gmail.com>
2023-09-22 17:34:38 +0700

    refactor: compose function

--- index.js
+++ index.js
@@ -6,15 +6,17 @@ const join = (arr) => arr.join('');
 
 const compose =
   (...functions) =>
-  (arg) => {
-    console.log({ arg, functions });
+  // (arg) => {
+  //   console.log({ arg, functions });
 
-    let result = arg;
-    functions.reverse().forEach((func) => {
-      result = func(result);
-    });
-    return result;
-  };
+  //   let result = arg;
+  //   functions.reverse().forEach((func) => {
+  //     result = func(result);
+  //   });
+  //   return result;
+  // };
+  (arg) =>
+    functions.reduceRight((acc, func) => func(acc), arg);
 
 const value = compose(join, reverse, split)('hello');
 
